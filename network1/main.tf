37db51e4175057f3baf5d417c5239c425155c40d
nyashamh <nyashamhishi@gmail.com>
2021-10-20 15:13:24 +0200

    added tags

--- network1/main.tf
+++ network1/main.tf
@@ -1,6 +1,10 @@
 resource "aws_vpc" "icdl-vpc-euw1" {
     cidr_block = "192.168.0.0/24"
     instance_tenancy = "default"
+        tags                             = {
+        "Creator" = "hamish@cloud-fundis"
+        "Name"    = "icdl-vpc-euw1"
+    }
 
 }
 
@@ -8,18 +12,30 @@ resource "aws_vpc" "icdl-vpc-euw1" {
 resource "aws_subnet" "icdl-pub-0a" {
     vpc_id = "aws_vpc.icdl-vpc-euwl.id"
     cidr_block = "192.168.0.0/27"
+        tags                            = {
+        "Creator" = "hamish@cloud-fundis"
+        "Name"    = "icdl-pub-0a"
+    }
   
 }
 
 resource "aws_subnet" "icdl-pub-1b" {
     vpc_id = "aws_vpc.icdl-vpc-euw1.id"
     cidr_block = "192.168.0.32/27"
+        tags                            = {
+        "Creator" = "nyasha@cloud-fundis"
+        "Name"    = "icdl-pub-1b"
+    }
   
 }
 
 resource "aws_subnet" "icdl-pub-1c" {
     vpc_id = "aws_vpc.icdl-vpc-euw1.id"
     cidr_block = "192.168.0.32/27"
+        tags                            = {
+        "Creator" = "nyasha@cloud-fundis"
+        "Name"    = "icdl-pub-1c"
+    }
   
 }
 
@@ -27,16 +43,28 @@ resource "aws_subnet" "icdl-pub-1c" {
 resource "aws_subnet" "icdl-pvt-0a" {
     vpc_id = "aws_vpc.icdl-vpc-euw1.id"
     cidr_block = "192.168.0.128/27"
+        tags                            = {
+        "Creator" = "nyasha@cloud-fundis"
+        "Name"    = "icdl-pvt-0a"
+    }
   
 }
 
 resource "aws_subnet" "icdl-pvt-1b" {
     vpc_id = "aws_vpc.icdl-vpc-euw1.id"
     cidr_block = "192.168.0.192/27"
+        tags                            = {
+        "Creator" = "nyasha@cloud-fundis"
+        "Name"    = "icdl-pvt-1b"
+    }
   
 }
 resource "aws_subnet" "icdl-pvt-1c" {
     vpc_id = "aws_vpc.icdl-vpc-euw1.id"
     cidr_block = "192.168.0.224/27"
+       tags                            = {
+        "Creator" = "nyasha@cloud-fundis"
+        "Name"    = "icdl-pvt-1c"
+    }
   
 }
