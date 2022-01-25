e124a933f09b895e66290d897587d45e3faae92a
nyashamh <nyashamhishi@gmail.com>
2022-01-25 15:42:21 +0200

    cleaned unrequired policies from codebuild

--- codebuild/codebuild-iam.tf
+++ codebuild/codebuild-iam.tf
@@ -33,74 +33,7 @@ resource "aws_iam_role_policy" "CodeBuildBasePolicy-icdl-3-eu-west-1" {
       ]
     },
 
-        {
-            "Effect": "Allow",
-            "Resource": "*",
-            "Action": [
-                "s3:PutObject",
-                "s3:GetObject",
-                "s3:GetObjectVersion",
-                "s3:GetBucketAcl",
-                "s3:GetBucketLocation"
-            ]
-        },
-        {
-            "Sid": "S3AccessPolicy",
-            "Effect": "Allow",
-            "Action": [
-                "s3:CreateBucket",
-                "s3:GetObject",
-                "s3:List*",
-                "s3:PutObject"
-            ],
-            "Resource": "*"
-        },
-        {
-            "Sid": "SpecificTable",
-            "Effect": "Allow",
-            "Action": [
-                "dynamodb:BatchGet*",
-                "dynamodb:DescribeStream",
-                "dynamodb:DescribeTable",
-                "dynamodb:Get*",
-                "dynamodb:Query",
-                "dynamodb:Scan",
-                "dynamodb:BatchWrite*",
-                "dynamodb:CreateTable",
-                "dynamodb:Delete*",
-                "dynamodb:Update*",
-                "dynamodb:PutItem"
-            ],
-            "Resource": "arn:aws:dynamodb:eu-west-1:813260210012:table/icdl-north-sstate-table"
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "lambda:AddPermission",
-                "lambda:RemovePermission",
-                "lambda:CreateAlias",
-                "lambda:UpdateAlias",
-                "lambda:DeleteAlias",
-                "lambda:UpdateFunctionCode",
-                "lambda:UpdateFunctionConfiguration",
-                "lambda:PutFunctionConcurrency",
-                "lambda:DeleteFunctionConcurrency",
-                "lambda:PublishVersion",
-                "lambda:GetFunction"
-            ],
-            "Resource": "arn:aws:iam::813260210012:role/service-role/codebuild-icdl-codebuild-service-role"
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "codebuild:CreateProject",
-                "codebuild:StartBuild",
-                "codebuild:StopBuild",
-                "codebuild:ListProjects"
-            ],
-            "Resource": "*"
-        },
-
+ 
         {
             "Effect": "Allow",
             "Action": [
@@ -308,11 +241,3 @@ resource "aws_iam_role_policy" "CodeBuildBasePolicy-icdl-3-eu-west-1" {
 }
 POLICY
 }
-
-/*resource "aws_iam_role_policy" "CodeBuildBasePolicy-icdl-3-eu-west-1" {
-  role = aws_iam_role.CodeBuildRole.name
-
-  policy = <<POLICY
-
-  POLICY
-}*/
